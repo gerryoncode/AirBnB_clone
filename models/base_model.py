0aea4bcf7dab94352e6e5b53095b9d555a8abd2a
christophersesugh <christohybrid185@gmail.com>
2023-07-14 14:31:36 +0000

    feat: add necessary folder and files for task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
     BaseModel that defines all common attriutes/methods for other classes
 """
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import unittest
 from datetime import datetime
 from unittest.mock import patch
